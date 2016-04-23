a15ea003dd558be5a1ba8b7e1b39aa11cfd59e63
Arthur Carabott <arthurc@gmail.com>
2016-04-24 08:32:32 +0900

    add template files

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
+
+gulp.task('babel', function() {
+  return gulp.src(['src/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .on('error', function() {
+      console.log('error!');
+      this.emit('end');
+    })
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dst'))
+    .pipe(livereload());
+});
+
+gulp.task('default', function() {
+  livereload.listen();
+  gulp.watch('src/*.js', ['babel']);
+});
